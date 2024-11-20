788bdf9fbb004ca2d37281f54d699fc1a85f5795
Farino Joshua <96770446+farinojoshua@users.noreply.github.com>
2024-11-20 10:26:37 +0700

    set middleware

---
+++ internal/middleware/middleware.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"errors"
+	"net/http"
+	"situs-forum/internal/configs"
+	"situs-forum/pkg/jwt"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	secretKey := configs.Get().Service.SecretJWT
+
+	return func(ctx *gin.Context) {
+		header := ctx.Request.Header.Get("Authorization")
+
+		header = strings.TrimSpace(header)
+		if header == "" {
+			ctx.AbortWithError(http.StatusUnauthorized, errors.New("Missing token"))
+			return
+		}
+
+		userId, username, err := jwt.ValidateToken(header, secretKey)
+		if err != nil {
+			ctx.AbortWithError(http.StatusUnauthorized, err)
+			return
+		}
+
+		ctx.Set("userID", userId)
+		ctx.Set("username", username)
+		ctx.Next()
+	}
+}
